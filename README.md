8234bc5a3dea161357dbbb5a0721c9d7df67bb1a
Tamoya Hopkins <tamoya@250ok.com>
2020-02-29 09:38:56 -0500

    fixing folder structure

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# twitter_clone
+# twitterclone
--- twitterclone/twitterclone/asgi.py
+++ twitterclone/twitterclone/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for twitter_clone project.
+ASGI config for twitterclone project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'twitter_clone.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'twitterclone.settings')
 
 application = get_asgi_application()
